1d1e30b44200a4aa685d42cfdd7902fb7a5575f1
Stephane BARON <sbaron@galilee.fr>
2017-12-29 08:50:23 +0100

    Clean images

--- 8/resources/entrypoint.sh
+++ 8/resources/entrypoint.sh
@@ -2,17 +2,12 @@
 
 set -e
 
-# Custom NPM config 
-if [ -n "$CI_NPM_CONFIG_EMAIL" ]; then
-	npm config set email "${CI_NPM_CONFIG_EMAIL}"
-fi
-
-if [ -n "$CI_NPM_CONFIG_REGISTRY" ]; then
+if [ -n "$NPM_CUSTOM_REGISTRY" ]; then
 	npm config set registry "${CI_NPM_CONFIG_REGISTRY}"
 fi
 
-if [ -n "$CI_NPM_CONFIG_USER" -a -n "$CI_NPM_CONFIG_PASSWORD" ]; then
-	npm config set _auth $(echo -n "$CI_NPM_CONFIG_USER:$CI_NPM_CONFIG_PASSWORD"|base64)
+if [ -n "$CI_NPM_CUSTOM_REGISTRY_USER" -a -n "$NPM_CUSTOM_REGISTRY_PASSWORD" ]; then
+	npm config set _auth $(echo -n "$CI_NPM_CUSTOM_REGISTRY_USER:$NPM_CUSTOM_REGISTRY_PASSWORD"|base64)
 	npm config set always-auth true
 fi
 
