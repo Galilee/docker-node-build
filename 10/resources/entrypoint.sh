a35c7c9d233aaa9a6b67a016bb99e53b9cba89b7
Stephane BARON <sbaron@galilee.fr>
2019-01-25 14:19:38 +0100

    Add node 10 / 11

---
+++ 10/resources/entrypoint.sh
@@ -0,0 +1,31 @@
+#! /bin/bash
+
+set -e
+
+# SSH key
+if [ -n "$SSH_PRIVATE_KEY" ]; then
+	eval $(ssh-agent -s)
+	ssh-add <(echo "$SSH_PRIVATE_KEY") >/dev/null 2>&1
+	unset SSH_PRIVATE_KEY
+fi
+
+if [ -n "$NPM_CUSTOM_REGISTRY" ]; then
+	npm config set registry "${NPM_CUSTOM_REGISTRY}"
+fi
+
+if [ -n "$NPM_CUSTOM_REGISTRY_USER" -a -n "$NPM_CUSTOM_REGISTRY_PASSWORD" ]; then
+	npm config set _auth $(echo -n "$NPM_CUSTOM_REGISTRY_USER:$NPM_CUSTOM_REGISTRY_PASSWORD"|base64)
+	npm config set always-auth true
+fi
+
+# Fix cache location
+if [ -n "$NPM_CONFIG_CACHE" ]; then
+	npm config set cache "${NPM_CONFIG_CACHE}"
+fi
+
+# Fix email
+if [ -n "$NPM_EMAIL" ]; then
+	npm config set email "${NPM_EMAIL}"
+fi
+
+exec "$@"
